d73833a5403e10b31ce3f3d5c73fc766122d5ce1
brito <jbrito712@gmail.com>
2016-12-14 01:19:28 +0000

    Initial

---
+++ tpf/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="tpf.Global" Language="C#" %>
---
+++ tpf/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tpf.Startup))]
+namespace tpf
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
